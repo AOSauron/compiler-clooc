0b481fbca78a7964ecbb6f9bf3c30425cbdf7e8e
AOSauron <guillaume.garcia@telecomnancy.net>
2017-03-09 01:02:12 +0100

    TDS continuacion

--- analyseurs/TreeParser.java
+++ analyseurs/TreeParser.java
@@ -11,12 +11,18 @@ public class TreeParser {
     this.ast=ast;
   }
 
+  /*
+   * Lance l'exploration de tout l'arbre !
+   */
   public void init() {
     System.out.println("Tree to parse : " + this.ast.toStringTree());
     table = new HashMap<String,LinkedList>();
     explorer(ast);
   }
 
+  /*
+   * Explorateur récursif de sous-arbre. Effectue des contrôles sémantiques !
+   */
   public void explorer(CommonTree tree) {
 
     int nbchlid = tree.getChildCount();
@@ -28,20 +34,39 @@ public class TreeParser {
       infos.add(tree.getChild(1));
       infos.add(null);
       table.put(tree.getChild(0).getText(),infos);
-      //System.out.println("TDS " + table.toString());
     }
+
+    // FOR
+    /*
+    if (tree.getText().equals("FOR")) {
+      LinkedList infos = new LinkedList();
+      infos.add(tree.getChild(1));
+      infos.add(null);
+      table.put(tree.getChild(0).getText(),infos);
+    } */
 
     // AFFECT
     if (tree.getText().equals("AFFECT")) {
       LinkedList infos = table.get(tree.getChild(0).getText());
       Object value;
-      if (infos.getFirst().equals("INT")) {
-        value = Integer.parseInt(tree.getChild(1).getText());
-      } else  {
+      int nbchlidnode = tree.getChild(1).getChildCount();
+
+      //System.out.println("DEBUG : " tree.getChild(0).getText() + nbchlidnode + infos);
+      // Cas d'un int, on parse directement en int
+      if (infos.getFirst().toString().equals("INT")) {
+        if (nbchlidnode > 0) {
+          //Cas d'une expression arithm/logique
+          value = calculator((CommonTree) tree.getChild(1));
+        } else {
+          //Cas d'un int simple
+          value = Integer.parseInt(tree.getChild(1).getText());
+        }
+      }
+      // Les autres cas, on parse en String.
+      else  {
         value = tree.getChild(1).getText();
       }
       infos.set(1, value);
-      //System.out.println("AFFECT " + table.toString());
     }
 
     if (nbchlid==0) {
@@ -53,5 +78,69 @@ public class TreeParser {
         explorer((CommonTree) tree.getChild(k));
       }
     }
+  }
+
+  /*
+   * Calulette récursive du compilateur, résoud les expressions arithmétiques/logiques.
+   */
+  public int calculator(CommonTree expr) {
+    int res;
+    res = 0; // A virer plus tard !
+
+    // Arrêt de la récursion : test d'abord si c'est un entier, ensuite s'il s'agit d'une variable de la TDS
+    if (expr.getChildCount()==0) {
+      try {
+        res = Integer.parseInt(expr.getText());
+        return res;
+      }
+      catch (Exception e) {
+       LinkedList infos = table.get(expr.getText());
+       res = (int) infos.get(1);
+      }
+    }
+
+    //Cas de l'opérateur unaire -
+    if (expr.getText().equals("-")) {
+      res = - calculator((CommonTree) expr.getChild(0));
+    }
+    // Logique ==     :   1 = true; 0 = false
+    if (expr.getText().equals("==")) {
+      res =  (calculator((CommonTree) expr.getChild(0)) == calculator((CommonTree) expr.getChild(1))) ? 1 : 0 ;
+    }
+    // Logique >=     :   1 = true; 0 = false
+    if (expr.getText().equals(">=")) {
+      res =  (calculator((CommonTree) expr.getChild(0)) >= calculator((CommonTree) expr.getChild(1))) ? 1 : 0 ;
+    }
+    // Logique <=     :   1 = true; 0 = false
+    if (expr.getText().equals("<=")) {
+      res =  (calculator((CommonTree) expr.getChild(0)) <= calculator((CommonTree) expr.getChild(1))) ? 1 : 0 ;
+    }
+    // Logique >     :   1 = true; 0 = false
+    if (expr.getText().equals(">")) {
+      res =  (calculator((CommonTree) expr.getChild(0)) > calculator((CommonTree) expr.getChild(1))) ? 1 : 0 ;
+    }
+    // Logique <     :   1 = true; 0 = false
+    if (expr.getText().equals("<")) {
+      res =  (calculator((CommonTree) expr.getChild(0)) < calculator((CommonTree) expr.getChild(1))) ? 1 : 0 ;
+    }
+    // Logique !=     :   1 = true; 0 = false
+    if (expr.getText().equals("!=")) {
+      res =  (calculator((CommonTree) expr.getChild(0)) != calculator((CommonTree) expr.getChild(1))) ? 1 : 0 ;
+    }
+    //Arithmétique +
+    if (expr.getText().equals("+")) {
+      res =  (calculator((CommonTree) expr.getChild(0)) + calculator((CommonTree) expr.getChild(1)));
+    }
+    //Arithmétique -
+    if (expr.getText().equals("-")) {
+      res =  (calculator((CommonTree) expr.getChild(0)) - calculator((CommonTree) expr.getChild(1)));
+    }
+    //Arithmétique *
+    if (expr.getText().equals("*")) {
+      res =  (calculator((CommonTree) expr.getChild(0)) * calculator((CommonTree) expr.getChild(1)));
+    }
+
+
+    return res;
   }
 }
--- analyseurs/testtable.sh
+++ analyseurs/testtable.sh
@@ -5,4 +5,4 @@ then
   java -jar ../ANTLR/antlr-3.3-complete.jar Looc.g #&> /dev/null
   javac -encoding ISO-8859-1 LoocLexer.java LoocParser.java TreeParser.java Clooc.java #&> /dev/null
 fi
-java Clooc -T ../prog_test/for.looc ../AST/testAST.dot
+java Clooc -T ../prog_test/progtestlvl1.looc ../AST/testAST.dot
