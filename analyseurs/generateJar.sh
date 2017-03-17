8cdc44371685633fd199a310d62ef3ec045d33a4
AOSauron <guillaume.garcia@telecomnancy.net>
2017-03-17 17:36:40 +0100

    Technique de release en jar stand-alone au point, automatisé par script shell - OK

---
+++ analyseurs/generateJar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "[RELEASING JAR] Recompilation générale ..."
+java -jar ../ANTLR/antlr-3.3-complete.jar Looc.g && javac *.java
+echo "[RELEASING JAR] Formation du Jar clooc.jar SANS les lib antlr et asmMicroPIUP ..."
+jar cvmf MANIFEST.MF clooc.jar *.class > /dev/null
+echo "[RELEASING JAR] Copie de ce jar dans le répertoire build/"
+mv -v clooc.jar ../build/clooc/main
+cd ../build/clooc
+echo "[RELEASING JAR] Production du jar final exécutable à la racine du dépot ..."
+jar -cvfm ../../clooc.jar boot-manifest.mf . > /dev/null
+cd - > /dev/null
+echo "[RELEASING JAR] Terminé. Pour lancer : java -jar clooc.jar [-T] [FICHIER]"
